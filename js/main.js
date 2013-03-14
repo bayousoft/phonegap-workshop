4b2ec0ded8b1c784b326746b6390565d468d5725
Ryan P LeTulle <bayousoft@gmail.com>
2013-03-14 14:40:43 -0500

    Use native notification

--- js/main.js
+++ js/main.js
@@ -13,8 +13,17 @@ var app = {
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     initialize: function() {
         this.store = new LocalStorageStore();
+            self.showAlert('Store Initialized', 'Info');
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
