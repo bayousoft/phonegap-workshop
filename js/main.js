2509add4808dc7edf41c3fef3513e350eec0a4e2
Ryan P LeTulle <bayousoft@gmail.com>
2013-04-01 17:27:45 -0500

    Testing native notifications

--- js/main.js
+++ js/main.js
@@ -14,7 +14,9 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+        this.store = new WebSqlStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
