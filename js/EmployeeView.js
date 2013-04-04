d57432b91e5fc4432f9a77236c2693f5e9f73185
Ryan P LeTulle <bayousoft@gmail.com>
2013-04-04 13:55:40 -0500

    Contacts API T-shooting

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -21,7 +21,7 @@ var EmployeeView = function(employee) {
 	        return;
 	    }
 	    var contact = navigator.contacts.create();
-	    contact.name = {'displayName': employee.firstName, familyName: employee.lastName};
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
 	    var phoneNumbers = [];
 	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
 	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
@@ -29,7 +29,7 @@ var EmployeeView = function(employee) {
 	    contact.save();
 	    return false;
 	};
-
+    
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
